029ecd10ffb2e669ad9cdf68e7a2a2bc309e3cd6
Ahmadfaisal04 <ahmadfaizal0328@gmail.com>
2024-05-18 12:13:04 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: 'column' }}>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ flex: 1, backgroundColor: 'yellow' }}></View>
+        <View style={{ flex: 1, backgroundColor: 'green' }}></View>
+      </View>
+      <View style={{ flex: 1, backgroundColor: 'blue' }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import NewPage from './NewPage';
+
+
+const NewPage = () => {
+    return (
+      <View>
+        <Text>Ini adalah halaman baru!</Text>
+      </View>
+    );
+  }
+  
+  export default NewPage;
